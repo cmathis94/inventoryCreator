27e576ed1b9927a77d9b83a2e3ece85fc536a98f
cmathis94 <collin.mathis94@gmail.com>
2022-10-24 15:56:48 -0500

    back at it

---
+++ models/inventory.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const InventorySchema = new Schema({
+  serial: String,
+  username: String,
+  make: String,
+  model: String,
+  warranty: Number,
+});
+
+module.exports = mongoose.model("Inventory", InventorySchema);
